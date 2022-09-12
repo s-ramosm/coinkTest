b83af4da0d69843277d1a9ad662c8a1b8b182b5f
= <saircontactoramos@gmail.com>
2022-09-12 00:01:23 -0500

    Agregando errores 2

--- coinkTest/urls.py
+++ coinkTest/urls.py
@@ -19,6 +19,7 @@ from users.views import parzival
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('users.urls')),
     path('users/' , include('users.urls')),
     path('parzival/', parzival, name='desarrollador'),
 ]
